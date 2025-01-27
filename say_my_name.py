33416319349cc51725df1aab6b31bf8082312762
Milena Davudova <mdavudova22@gmail.com>
2025-01-27 20:12:48 +0000

    EDIT: milena's code

--- say_my_name.py
+++ say_my_name.py
@@ -2,3 +2,6 @@ first_name="Kinga"
 last_name="Kisielewska"
 print(first_name,last_name)
 #created two variables and concatenated with a comma for a spaced output
+first_name="Milena"
+last_name="Davudova"
+print(first_name, last_name)
