89d02719e42999c404aff78b62a504b71f8b0d2e
Kinga Kisielewska <kkisielewska9@gmail.com>
2025-01-28 18:26:45 +0000

    EDIT: tidied spacing

--- operations.py
+++ operations.py
@@ -2,7 +2,7 @@
 var = input("Please enter a value:")
 #creating a var function which asks user for input
 
-capitalised_var =var.capitalize()
+capitalised_var = var.capitalize()
 #creating a variable called capitalised_var which stores var changed through a capitalize function??
 
 print(capitalised_var)
